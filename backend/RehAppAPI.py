9e6c9360a8972ed0410ac94109247dabb81f30f4
Jakub1999B <jakubbascik1999@gmail.com>
2023-08-15 22:15:14 +0200

    Display trainings table

--- backend/RehAppAPI.py
+++ backend/RehAppAPI.py
@@ -1,18 +1,25 @@
 import pandas as pd
-from fastapi import FastAPI, UploadFile, File
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 from prediction.reh_app import reh_app, plot_data
 from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import create_engine, Column, Integer, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import json
+import contextlib
 from datetime import datetime
 from fastapi.staticfiles import StaticFiles
-from pathlib import Path
+import json
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from pandas import DataFrame
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import HTMLResponse
+import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -22,11 +29,20 @@ DATABASE_URL = "sqlite:///trainings.db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
-import json
-import numpy as np
-from pandas import DataFrame
-from fastapi.encoders import jsonable_encoder
+# Template Setup
+templates = Jinja2Templates(directory="templates")
+
+from fastapi import Depends
+
+# Define a dependency function to manage the database session
+def get_db_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class TrainingRecord(Base):
@@ -41,10 +57,6 @@ class TrainingRecord(Base):
     side_bend = Column(Integer)
     duration = Column(Integer)
 
-Base.metadata.create_all(bind=engine)
-
-# Template Setup
-templates = Jinja2Templates(directory="templates")
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -62,80 +74,92 @@ async def read_root():
         content = f.read()
     return content
 
-@app.get("/{template}", response_class=HTMLResponse)
-async def read_template(template: str):
-    if template in ["trainings", "analyze", "exercises", "contact"]:
-        with open(f"templates/{template}.html", "r") as f:
-            content = f.read()
-        return content
-    else:
-        return "Template not found"
+# @app.get("/{template}", response_class=HTMLResponse)
+# async def read_template(template: str):
+#     if template in ["analyze", "exercises", "contact"]:
+#         with open(f"templates/{template}.html", "r") as f:
+#             content = f.read()
+#         return content
+#     else:
+#         return "Template not found"
 
-@app.post("/analyzes/")
-async def analyze(file: UploadFile = File(...)):
-    df, summary_count, duration = reh_app(file.file)
-    encoded_content = json.dumps(df, cls=CustomJSONEncoder)
-    f = pd.read_excel(file.file)
+@app.get("/analyze", response_class=HTMLResponse)
+async def show_analyze():
+    with open(f"templates/analyze.html", "r") as f:
+        content = f.read()
+    return content
 
-    # Example of how to insert records into the database
-    new_record = TrainingRecord(
-        bend=summary_count.get('BEND', 0),
-        circular_raise=summary_count.get('CIRCULAR_RAISE', 0),
-        abduction=summary_count.get('ABDUCTION', 0),
-        rear_touch=summary_count.get('REAR_TOUCH', 0),
-        side_bend=summary_count.get('SIDE_BEND', 0),
-        duration=duration
-    )
-
-    db = SessionLocal()
-    db.add(new_record)
-    db.commit()
-    db.close()
-
-    return JSONResponse(content=encoded_content)
-
-
-from fastapi.responses import HTMLResponse
-from fastapi.requests import Request
-
-# @app.get("/analyze/", response_class=HTMLResponse)
-# async def show_analyze_page(request: Request):
-#     return templates.TemplateResponse("analyze.html", {"request": request})
+# @app.post("/analyzes/")
+# async def analyze(file: UploadFile = File(...)):
+#     df, summary_count, duration = reh_app(file.file)
+#     encoded_content = json.dumps(df, cls=CustomJSONEncoder)
+#     f = pd.read_excel(file.file)
 #
+#     # Example of how to insert records into the database
+#     new_record = TrainingRecord(
+#         bend=summary_count.get('BEND', 0),
+#         circular_raise=summary_count.get('CIRCULAR_RAISE', 0),
+#         abduction=summary_count.get('ABDUCTION', 0),
+#         rear_touch=summary_count.get('REAR_TOUCH', 0),
+#         side_bend=summary_count.get('SIDE_BEND', 0),
+#         duration=duration
+#     )
 #
-# @app.get("/trainings/", response_class=HTMLResponse)
-# async def show_trainings(request: Request):
 #     db = SessionLocal()
-#     records = db.query(TrainingRecord).all()
+#     db.add(new_record)
+#     db.commit()
 #     db.close()
-#     return templates.TemplateResponse("trainings.html", {"request": request, "records": records})
-
-
-
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
-import os
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-# @app.get("/styles.css")
-# async def get_styles():
-#     return FileResponse("static/styles.css")
 #
-# @app.get("/static/logo.png")
-# async def get_styles():
-#     return FileResponse("static/logo.png")
+#     return JSONResponse(content=encoded_content)
+
+@app.get("/trainings", response_class=HTMLResponse)
+async def trainings(request: Request):
+    try:
+        db = SessionLocal()
+        records = db.query(TrainingRecord).all()
+        print("Records", records)
+        return templates.TemplateResponse(
+            "trainings.html",
+            {"request": request, "records": records},
+        )
+    except Exception as e:
+        print("Error:", e)
+        raise
+    finally:
+        db.close()
 
 
 
-# @app.get("/load-template/{template_name}", response_class=HTMLResponse)
-# async def load_template(template_name: str, request: Request):
-#     templates_old = Jinja2Templates(directory="templates_old")
-#     print(templates_old)
-#     print(template_name)
-#     print(templates_old.TemplateResponse(f"{template_name}.html", {"request": request}))
-#     return templates_old.TemplateResponse(f"{template_name}.html", {"request": request})
+
+@app.post("/analyzes/", response_class=HTMLResponse)
+async def analyze(request: Request, file: UploadFile = File(...)):
+    try:
+        db = SessionLocal()
+        df, summary_count, duration = reh_app(file.file)
+        encoded_content = json.dumps(df, cls=CustomJSONEncoder)
+
+        new_record = TrainingRecord(
+            bend=summary_count.get('BEND', 0),
+            circular_raise=summary_count.get('CIRCULAR_RAISE', 0),
+            abduction=summary_count.get('ABDUCTION', 0),
+            rear_touch=summary_count.get('REAR_TOUCH', 0),
+            side_bend=summary_count.get('SIDE_BEND', 0),
+            duration=duration
+        )
+
+        db.add(new_record)
+        db.commit()
+
+        return templates.TemplateResponse(
+            "analyze.html",
+            {"request": request, "encoded_content": encoded_content},
+        )
+    except Exception as e:
+        print("Error:", e)
+        raise
+    finally:
+        db.close()
+
 
 
 
